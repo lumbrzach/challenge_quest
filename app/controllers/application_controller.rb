8a28429c6b631e60843bb8624dcca6e116ef0c65
github username <github email address>
2019-07-09 11:23:10 -0400

    added login, adjusted routes, adjusted some views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,3 @@
 class ApplicationController < ActionController::Base
-    def home
-        render :root
-    end
+    
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,2 +1,12 @@
 class RelationshipsController < ApplicationController
+    def create
+        @relationship = current_user.relationships.build(:friend_id => params[:friend_id])
+        if @relationship.save
+          flash[:notice] = "Added friend."
+          redirect_to users_path
+        else
+          flash[:notice] = "Unable to add friend."
+          redirect_to users_path
+        end
+      end
 end
