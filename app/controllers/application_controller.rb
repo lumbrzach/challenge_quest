cca2aa7c6819f0e4b87f36afcaec89159f823c12
github username <github email address>
2019-07-09 13:53:48 -0400

    got basic login working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
+    def current_user
+        user_id = session[:user_id]
+        user_id && User.find(user_id)
+      end
     
+      def authenticate!
+        unless current_user
+          flash[:info] = "You have to be logged in to do that!"
+          redirect_to challenges_path
+        end
+      end
 end
