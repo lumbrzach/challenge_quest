4b63026dd579bb37541c0814af1abaf7bea2ecf4
github username <github email address>
2019-07-09 14:18:30 -0400

    updated challenge relationships

--- app/models/challenge.rb
+++ app/models/challenge.rb
@@ -1,2 +1,4 @@
 class Challenge < ApplicationRecord
+    has_many :attempts
+    has_many :users, through: :attempts
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,6 @@ class User < ApplicationRecord
     validates :name, presence: true
     validates :username, presence: true, uniqueness: true
     has_secure_password
+
+    
 end
