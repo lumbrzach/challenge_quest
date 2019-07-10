e4b867a4d1e239b5573399646d2eeb4b5cc7084b
github username <github email address>
2019-07-10 16:14:54 -0400

    did many things

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -1,11 +1,11 @@
 class RelationshipsController < ApplicationController
     def create
         @relationship = current_user.relationships.build(:friend_id => params[:friend_id])
-        if @relationship.save
-          flash[:notice] = "Added friend."
+        if @relationship.uniq && @relationship.save
+          flash[:notice] = "Added Friend."
           redirect_to users_path
         else
-          flash[:notice] = "Unable to add friend."
+          flash[:notice] = "Unable to Add Friend."
           redirect_to users_path
         end
       end
@@ -13,7 +13,7 @@ class RelationshipsController < ApplicationController
       def destroy
         @relationship= current_user.relationships.find(params[:id])
         @relationship.destroy
-        flash[:notice] = "Removed friendship."
+        flash[:notice] = "Removed Friend."
         redirect_to current_user
       end
 end
