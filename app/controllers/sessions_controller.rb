a73f23d432e94dc6a65f792d4aa51fa682a3ae11
Dylan Harris <dylanharris23@gmail.com>
2019-07-10 11:45:16 -0400

    checkpoint

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -17,6 +17,6 @@ class SessionsController < ApplicationController
 
       def destroy 
         session[:user_id] = nil
-        redirect_to '/' :notice => "You Have Been Logged Out" 
+        redirect_to '/' 
       end
 end
