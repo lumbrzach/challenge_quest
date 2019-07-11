6c21f6689ed4b27a0f851fd262336410be40fe4a
Dylan Harris <dylanharris23@gmail.com>
2019-07-11 08:35:20 -0400

    Navbar links done

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :achievements
   get '/', to: 'sessions#new'
   post '/', to: 'sessions#create'
-  delete '/logout', to: 'sessions#destroy'
+  get '/logout', to: 'sessions#destroy'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
